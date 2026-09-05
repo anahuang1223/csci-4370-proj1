fcfbb4b74aa3efc65e0fd9e48fcedcb58bd32d55
sach-UGA <ss37550@uga.edu>
2026-09-05 18:23:23 -0400

    Update RAImpl.java
    Added union, intersect, and diff methods (v1)

--- src/main/java/uga/csx370/mydbimpl/RAImpl.java
+++ src/main/java/uga/csx370/mydbimpl/RAImpl.java
@@ -23,21 +23,87 @@ public class RAImpl implements RA {
     @Override
     public Relation union(Relation rel1, Relation rel2) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'union'");
+        int r1size = rel1.getSize();
+        int r2size = rel2.getSize();
+        if (!rel1.getTypes().equals(rel2.getTypes())) {
+            throw new IllegalArgumentException("Relations must have the same number of attributes and of the same type.");
+        } 
+        Relation combined = new RelationBuilder() 
+                .attributeNames(rel1.getAttrs())
+                .attributeTypes(rel1.getTypes())
+                .build();
+        for (int i=0; i<r1size; i++) {
+            combined.insert(rel1.getRow(i));
+        }
+        for (int i=0; i<r2size; i++) {
+            boolean dupe = false;
+            List<Cell> cur = rel2.getRow(i);
+            for (int j=0; j<r1size; j++) {
+                if (rel1.getRow(j).equals(cur)) {
+                    dupe = true;
+                }
+            }
+            if (!dupe) {
+                combined.insert(rel2.getRow(i));
+            }
+        }
+        return combined;
     }
 
     @Override
     public Relation intersect(Relation rel1, Relation rel2) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'intersect'");
+        int r1size = rel1.getSize();
+        int r2size = rel2.getSize();
+        if (!rel1.getTypes().equals(rel2.getTypes())) {
+            throw new IllegalArgumentException("Relations must have the same number of attributes and of the same type.");
+        } 
+        Relation combined = new RelationBuilder() 
+                .attributeNames(rel1.getAttrs())
+                .attributeTypes(rel1.getTypes())
+                .build();
+        for (int i=0; i<r2size; i++) {
+            boolean dupe = false;
+            List<Cell> cur = rel2.getRow(i);
+            for (int j=0; j<r1size; j++) {
+                if (rel1.getRow(j).equals(cur)) {
+                    dupe = true;
+                }
+            }
+            if (dupe) {
+                combined.insert(rel2.getRow(i));
+            }
+        }
+        return combined;
     }
 
     @Override
     public Relation diff(Relation rel1, Relation rel2) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'diff'");
+        int r1size = rel1.getSize();
+        int r2size = rel2.getSize();
+        if (!rel1.getTypes().equals(rel2.getTypes())) {
+            throw new IllegalArgumentException("Relations must have the same number of attributes and of the same type.");
+        } 
+        Relation combined = new RelationBuilder() 
+                .attributeNames(rel1.getAttrs())
+                .attributeTypes(rel1.getTypes())
+                .build();
+        for (int i=0; i<r1size; i++) {
+            boolean dupe = false;
+            List<Cell> cur = rel1.getRow(i);
+            for (int j=0; j<r2size; j++) {
+                if (rel2.getRow(j).equals(cur)) {
+                    dupe = true;
+                }
+            }
+            if (!dupe) {
+                combined.insert(rel1.getRow(i));
+            }
+        }
+        return combined;
     }
-
+    
     @Override
     public Relation rename(Relation rel, List<String> origAttr, List<String> renamedAttr) {
         // TODO Auto-generated method stub
