f714abb0dbea38f99710104ecf412f27984ec3f9
Ana Huang <anahuang1223@gmail.com>
2026-09-13 11:07:00 -0400

    added join pred test and query

--- src/main/java/uga/csx370/mydbimpl/Driver.java
+++ src/main/java/uga/csx370/mydbimpl/Driver.java
@@ -7,10 +7,11 @@
 package uga.csx370.mydbimpl;
 
 import java.util.List;
-
+import uga.csx370.mydb.Predicate;
 import uga.csx370.mydb.Relation;
 import uga.csx370.mydb.RelationBuilder;
 import uga.csx370.mydb.Type;
+import uga.csx370.mydb.RA;
 
 public class Driver {
     
@@ -22,11 +23,52 @@ public class Driver {
         // Path should be replaced with a correct file path for a compatible
         // CSV file.
         Relation rel1 = new RelationBuilder()
-                .attributeNames(List.of("Col01_Name", "Col02_Name", "Col03_Name"))
-                .attributeTypes(List.of(Type.INTEGER, Type.STRING, Type.DOUBLE))
+                .attributeNames(List.of("ID", "name", "dept_name", "salary"))
+                .attributeTypes(List.of(Type.INTEGER, Type.STRING, Type.STRING, Type.DOUBLE))
                 .build();
-        rel1.loadData("/path/to/exported/csv_file");
-        rel1.print();
+        rel1.loadData("/var/lib/mysql-files/instructor_export.csv");
+        //rel1.print();
+
+        Relation rel2 = new RelationBuilder()
+                .attributeNames(List.of("ID", "name", "dept_name", "tot_cred"))
+                .attributeTypes(List.of(Type.INTEGER, Type.STRING, Type.STRING, Type.INTEGER))
+                .build();
+        rel2.loadData("/var/lib/mysql-files/student_export.csv");
+        //rel2.print();
+
+        Relation course = new RelationBuilder()
+            .attributeNames(List.of("course_id", "course_title", "course_dept_name", "course_credits"))
+            .attributeTypes(List.of(Type.STRING, Type.STRING, Type.STRING, Type.INTEGER))
+            .build();
+        course.loadData("/var/lib/mysql-files/course_export.csv");
+
+        Relation teaches = new RelationBuilder()
+            .attributeNames(List.of("teaches_ID", "teaches_course_id", "teaches_sec_id", "teaches_semester", "teaches_year"))
+            .attributeTypes(List.of(Type.INTEGER, Type.STRING, Type.STRING, Type.STRING, Type.INTEGER))
+            .build();
+        teaches.loadData("/var/lib/mysql-files/teaches_export.csv");
+
+
+        System.out.println("Find the course ID and title of courses taught by instructors with a salary greater than 100,000.");
+        RA ra = new RAImpl();
+        
+        int instrIdIdx = rel1.getAttrIndex("ID");
+        int teachesIdIdx = rel1.getAttrs().size() + teaches.getAttrIndex("teaches_ID");
+        Predicate instrTeachesPred = row -> row.get(instrIdIdx).equals(row.get(teachesIdIdx));
+        // theta join
+        Relation instrTeaches = ra.join(rel1, teaches, instrTeachesPred);
+        int teachesCourseIdx = instrTeaches.getAttrIndex("teaches_course_id");
+        int courseIdIdx = instrTeaches.getAttrs().size() + course.getAttrIndex("course_id");
+        Predicate teachesCoursePred = row -> row.get(teachesCourseIdx).equals(row.get(courseIdIdx));
+        // theta join
+        Relation instrTeachesCourse = ra.join(instrTeaches, course, teachesCoursePred);
+        int salaryIdx = instrTeachesCourse.getAttrIndex("salary");
+        Predicate highSalary = row -> row.get(salaryIdx).getAsDouble() > 100000;
+        // select
+        Relation filtered = ra.select(instrTeachesCourse, highSalary);
+        // project
+        Relation answer = ra.project(filtered, List.of("course_id", "course_title"));
+        answer.print();
     }
 
 }
--- src/test/java/uga/csx370/AppTest.java
+++ src/test/java/uga/csx370/AppTest.java
@@ -3,6 +3,15 @@ package uga.csx370;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import java.util.List;
+import uga.csx370.mydb.Cell;
+import uga.csx370.mydb.Predicate;
+import uga.csx370.mydb.RA;
+import uga.csx370.mydb.Relation;
+import uga.csx370.mydb.RelationBuilder;
+import uga.csx370.mydb.Type;
+import uga.csx370.mydbimpl.RAImpl;
+
 
 /**
  * Unit test for simple App.
@@ -13,7 +22,24 @@ public class AppTest {
      * Example Test.
      */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testJoinPredicate() {
+        Relation rel1 = new RelationBuilder()
+                .attributeNames(List.of("ID", "name", "dept_name", "salary"))
+                .attributeTypes(List.of(Type.INTEGER, Type.STRING, Type.STRING, Type.DOUBLE))
+                .build();
+        rel1.loadData("/var/lib/mysql-files/instructor_export.csv");
+        Relation rel2 = new RelationBuilder()
+                .attributeNames(List.of("stud_ID", "stud_name", "stud_dept", "tot_cred"))
+                .attributeTypes(List.of(Type.INTEGER, Type.STRING, Type.STRING, Type.INTEGER))
+                .build();
+        rel2.loadData("/var/lib/mysql-files/student_export.csv");
+
+        RA test = new RAImpl();
+        Predicate same_dept = row -> row.get(2).getAsString()
+                .equals(row.get(6).getAsString());
+        Relation result = test.join(rel1, rel2, same_dept);
+        for (int i = 0; i < Math.min(10, result.getSize()); i++) {
+            System.out.println(result.getRow(i));
+        }
     }
 }
